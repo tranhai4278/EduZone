8c5e795661ba9a34048f3a7f0435e6a464ad9992
Quang Minh <minhdqhe163046@fpt.edu.vn>
2023-09-27 22:34:23 +0700

    fix

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -9,7 +9,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.List;
 import model.User;
 
 /**
--- src/java/model/Setting.java
+++ src/java/model/Setting.java
@@ -25,22 +25,13 @@ public class Setting {
     public Setting() {
     }
 
-<<<<<<< HEAD
-    public Setting(int settingId, int settingGroup, String settingName, boolean status, int displayOrder, String note, Date createAt, int createBy, Date updateAt, int updateBy) {
-=======
     public Setting(int settingId, int settingGroup, String settingName, boolean status, int displayOrder, String note, Date updateAt, int updateBy) {
->>>>>>> f1448c2fcd7f0ec75b808d3c1521a7c1f8840b67
         this.settingId = settingId;
         this.settingGroup = settingGroup;
         this.settingName = settingName;
         this.status = status;
         this.displayOrder = displayOrder;
         this.note = note;
-<<<<<<< HEAD
-        this.createAt = createAt;
-        this.createBy = createBy;
-=======
->>>>>>> f1448c2fcd7f0ec75b808d3c1521a7c1f8840b67
         this.updateAt = updateAt;
         this.updateBy = updateBy;
     }
@@ -150,17 +141,7 @@ public class Setting {
     public void setUpdateBy(int updateBy) {
         this.updateBy = updateBy;
     }
-<<<<<<< HEAD
-
-    @Override
-    public String toString() {
-        return "Setting{" + "settingId=" + settingId + ", settingGroup=" + settingGroup + ", settingName=" + settingName + ", status=" + status + ", displayOrder=" + displayOrder + ", note=" + note + ", createAt=" + createAt + ", createBy=" + createBy + ", updateAt=" + updateAt + ", updateBy=" + updateBy + '}';
-    }
-
-    
-
-=======
->>>>>>> f1448c2fcd7f0ec75b808d3c1521a7c1f8840b67
-    
 
 }
+    
+

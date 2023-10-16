7f57cf77a7f03249f7a8800285d3aaa8b9df5586
Bui Hai Nam <nambhhe176453@fpt.edu.vn>
2023-10-17 00:17:48 +0700

    add security filter for many page

--- src/java/Login/controller/RegisterController.java
+++ src/java/Login/controller/RegisterController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package Login.controller;
 
 import dal.AdminDAO;
@@ -18,49 +14,14 @@ import java.util.logging.Logger;
 import model.User;
 import utils.MyUtil;
 
-/**
- *
- * @author MinhDQ
- */
 public class RegisterController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doPost(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -85,14 +46,14 @@ public class RegisterController extends HttpServlet {
         //New user
         Date d = new Date();
         User user = new User(md5, name, gender, phone, email, 4, true, d, 0, d, 1);
-        
+
         //Check the domain exists and is active
-        if (adminDAO.getDomainBySetting(dm,true)!= null) {
+        if (adminDAO.getDomainBySetting(dm, true) != null) {
             //Check email, phone exists, send email
             if (userDAO.getUserByEmail(email) != null) {
                 request.setAttribute("message_email", "Email already exists");
                 request.getRequestDispatcher("register.jsp").forward(request, response);
-            } else if (userDAO.getUserByPhone(phone) != null) { 
+            } else if (userDAO.getUserByPhone(phone) != null) {
                 request.setAttribute("message_phone", "Phone number already exists");
                 request.getRequestDispatcher("register.jsp").forward(request, response);
             } else {
@@ -107,22 +68,11 @@ public class RegisterController extends HttpServlet {
 
                 request.getRequestDispatcher("checkauthen.jsp").forward(request, response);
             }
-        }else {
+        } else {
             //Notification error exists
             request.setAttribute("message_domain", "Your email is not allowed to register an account");
             request.getRequestDispatcher("register.jsp").forward(request, response);
         }
 
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
--- src/java/model/Subject_setting.java
+++ src/java/model/Subject_setting.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.util.Date;
 
-/**
- *
- * @author Nết
- */
 public class Subject_setting {
     private int settingId;
     private int subjectId;

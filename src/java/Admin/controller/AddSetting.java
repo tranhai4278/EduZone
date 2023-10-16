291a1d8fa93c790a65d997e01ff14f70eb127f45
Bui Hai Nam <nambhhe176453@fpt.edu.vn>
2023-10-17 02:23:14 +0700

    clean code

--- src/java/Admin/controller/AddSetting.java
+++ src/java/Admin/controller/AddSetting.java
@@ -17,38 +17,9 @@ import model.User;
 @WebServlet(name = "AddSetting", urlPatterns = {"/addsetting"})
 public class AddSetting extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AccountServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AccountServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-
     }
 
     @Override
@@ -70,12 +41,6 @@ public class AddSetting extends HttpServlet {
             Setting s = new Setting(group, name, true, 0, note, timestamp, uid, timestamp, uid);
             dao.addSetting(s);
             response.sendRedirect("setting");
-
         }
-        else{
-            
-
-        }
-
     }
 }

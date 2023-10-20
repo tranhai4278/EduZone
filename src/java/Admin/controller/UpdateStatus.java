6f5bd2922a0610dce995639cd351093261edc8b6
Bui Hai Nam <nambhhe176453@fpt.edu.vn>
2023-10-20 12:56:04 +0700

    fix

--- src/java/Admin/controller/UpdateStatus.java
+++ src/java/Admin/controller/UpdateStatus.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet(name = "UpdateStatus", urlPatterns = {"/updateStatus"})

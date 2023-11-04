49498c2c27047dd4be7ec81ee5c86234587c04c6
ngocptahe172779 <ngocptahe172779@fpt.edu.vn>
2023-11-04 19:59:24 +0700

    commit

--- src/java/model/Lesson.java
+++ src/java/model/Lesson.java
@@ -182,5 +182,30 @@ public class Lesson {
     public void setUpdateBy(int updateBy) {
         this.updateBy = updateBy;
     }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+    
     
 }

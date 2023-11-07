79c91ab828177eceabc6b7c0d8d504e515c51224
unknown <ducdthe173428@fpt.edu.vn>
2023-11-07 22:14:43 +0700

    Fixed Conflicts

--- src/java/dal/AssignmentDAO.java
+++ src/java/dal/AssignmentDAO.java
@@ -12,11 +12,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-<<<<<<< HEAD
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-=======
->>>>>>> 1922bd7941c85c1d5c5b222540c0d48a0e6f1008
 import model.Assignment;
 
 /**
@@ -51,7 +48,6 @@ public class AssignmentDAO extends MySqlConnection {
         }
     }
 
-<<<<<<< HEAD
     public ArrayList getAllAssignmentTrainee(int user_id) {
         ArrayList<Assignment> list = new ArrayList<>();
         String sql = "SELECT * FROM `assignment` WHERE trainee_id=?";
@@ -110,10 +106,6 @@ public class AssignmentDAO extends MySqlConnection {
 
     public String getTraineeName(int id) {
         String query = "SELECT u.full_name FROM `assigment` a JOIN user u ON a.trainee_id = u.user_id WHERE a.trainee_id = ?";
-=======
-    public String getTraineeName(int id) {
-        String query = "SELECT u.full_name FROM `assignment` a JOIN user u ON a.trainee_id = u.user_id WHERE a.trainee_id = ?";
->>>>>>> 1922bd7941c85c1d5c5b222540c0d48a0e6f1008
         try {
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, id);
@@ -139,7 +131,6 @@ public class AssignmentDAO extends MySqlConnection {
 
     }
 
-<<<<<<< HEAD
     public boolean submitAssignment(int assignmentId, int traineeId, int classId, String fileName) {
         String sql = "UPDATE assignment SET submit_file = ?, submitTime = ?, status = ? WHERE assigment_id = ? AND trainee_id = ? AND class_id = ?";
         try {
@@ -166,8 +157,6 @@ public class AssignmentDAO extends MySqlConnection {
         }
     }
 
-=======
->>>>>>> 1922bd7941c85c1d5c5b222540c0d48a0e6f1008
     public void evaluateAssignment(double mark, String comment, String assignId, String classId, String traineeId) {
         MySqlConnection dbContext = new MySqlConnection();
         try {
@@ -187,7 +176,6 @@ public class AssignmentDAO extends MySqlConnection {
         }
     }
 
-<<<<<<< HEAD
     public ArrayList getAssignmentsWithDetails() {
         ArrayList<AssignmentDTO> data = new ArrayList<>();
 
@@ -264,28 +252,4 @@ public class AssignmentDAO extends MySqlConnection {
         return null;
     }
 
-=======
-    public ArrayList<Assignment> getAssignmentWithSIdAndClassId(int classId, int subjectId) {
-        ArrayList<Assignment> assignmentList = new ArrayList<>();
-        String sql = "SELECT a.assignment_id, a.class_id, a.trainee_id, a.submit_file, a.mark, a.weight, a.status, a.comment, a.submitTime\n"
-                + "FROM assignment a\n"
-                + "JOIN class c ON a.class_id = c.class_id\n"
-                + "JOIN subject s ON c.subject_id = s.subject_id\n"
-                + "WHERE c.class_id = " + classId + " AND s.subject_id = " + subjectId + ";";
-        
-        try {
-            statement = connection.prepareStatement(sql);
-            result = statement.executeQuery();
-            
-            while (result.next()) {
-                Assignment assignment = new Assignment();
-                
-            }
-            
-            return assignmentList;
-        } catch(Exception e) {
-            return null;
-        }
-    }
->>>>>>> 1922bd7941c85c1d5c5b222540c0d48a0e6f1008
 }
